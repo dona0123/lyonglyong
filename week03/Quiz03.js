21d4e5ff02a99f963128a52b1a20c3ea641a5a61
dona0123 <ehskmagic7@gmail.com>
2023-10-10 01:01:47 +0900

    [1010] React.js 스터디 week03

--- week03/Quiz03.js
+++ week03/Quiz03.js
@@ -1,10 +1,25 @@
-// 1. 현재 날짜 시간 
+// 1. 현재 날짜, 시간을 변수에 받은 후 달만 출력하시오. (달은 왜 1이 적은지 설명하시오.)
 
 let date = new Date(); 
 let month = date.getMonth(); 
-console.log(month);
+console.log(month+1);
 
-// 달은 0~11까지 있기 때문에 1이 적다 
+// 달은 0~11까지 있기 때문에 1이 적다. 
 
 
-// 2. 
+// 2. 프로미스 객체를 생성할 때 , “성공”하게 만들기 위한 2가지 빈칸을 채우시오.
+const promise = new Promise(
+	function (resolve, reject) {
+		setTimeout(() => {
+			// _____(1)____("성공");
+            resolve("성공")
+		}, 500);
+	}
+);
+
+// promise.___(2)___(function (res) {
+// 	console.log(res);
+// });
+promise.then(function (res) {
+	console.log(res);
+});
--- week03/Study03.js
+++ week03/Study03.js
@@ -63,3 +63,136 @@ function funrest(...rest) {
 
 funrest(1,2,3,4); 
 
+
+// ============================================= // 
+
+
+// 순회 메서드(forEach)
+const arr1  = [1,2,3]; 
+arr1.forEach(each);
+
+function each(item, idx) {
+    console.log(`${idx}의 값은 ${item}이다.`)
+}
+
+
+// ============================================= // 
+
+
+// indexOf (탐색 메서드)
+let arr2 = [1,3,5,7,1];
+
+console.log(arr2.indexOf(1,4)); // 인덱스 1을 찾고, 0부터 탐색해라.
+
+
+// ============================================= // 
+
+
+// findIndex 메서드 
+// true를 반환하는 첫 번째 요소의 인덱스를 반환
+function determine(item, idx, arr) { //2. 6이 참이면 탐색 stop!
+	if(item % 2 === 0) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+let arr3 = [1,3,5,6,8];
+let index1 = arr3.findIndex(determine); //1. determine의 참인지 판단
+
+console.log(index1); //3. index 값에 6의 인덱스 번호인 3 출력!
+
+
+// ============================================= // 
+
+
+// 화살표 함수 
+let arr4 = [1,3,5,6,8];
+let index2 = arr.findIndex((item) =>
+	item % 2 === 0 ? true : false
+);
+
+console.log(index2);
+
+
+// ============================================= // 
+
+
+// find 메서드: 요소 반환 
+let arr5 = [
+	{ name: "이종원", hobby: "축구" },
+	{ name: "이정환", hobby: "영화" },
+	{ name: "신다민", hobby: "축구" },
+	{ name: "김효빈", hobby: "노래" }
+];
+
+let filteredArr = arr5.filter(
+	(item) => item.hobby === "축구"
+);
+
+console.log(filteredArr);
+
+
+// ============================================= // 
+
+
+// reduce 메서드 
+let arr6 = [1, 2, 3, 4, 5];
+let result = arr6.reduce((acc, item) => acc + item);
+console.log(result); // 15
+
+
+// ============================================= // 
+
+
+// sort 메서드 
+let arr7 = [10, 5, 3];
+
+arr7.sort(function(a, b) {
+  return a - b; // 오름차순 정렬
+}); 
+
+// 음수를 반환하면 a를 b보다 앞으로 정렬합니다.
+// 양수를 반환하면 b를 a보다 앞으로 정렬합니다.
+// 0을 반환하면 a와 b의 순서를 변경하지 않습니다.
+
+console.log(arr); // [3, 5, 10]
+
+
+// ============================================= // 
+
+
+// Date 객체 
+function filterThisMonth(pivotDate, dateArray) {
+    const year = pivotDate.getFullYear();
+    const month = pivotDate.getMonth();
+
+    // 이번달에 가장 빠른 시간인 10월 1일 0시 0분 0초로 설정한다.
+    const startDay = new Date(year, month, 1, 0, 0, 0, 0);
+
+    // 이번달에 가장 마지막인 다음달 0일 23시 59분 59초로 설정한다.
+    const endDay = new Date(year, month + 1, 0, 23, 59, 59);
+
+    const resArr = dateArray.filter((it) =>
+        startDay.getTime() <= it.getTime() && it.getTime() <= endDay.getTime()
+    );
+    return resArr;
+}
+
+
+const dateArray = [
+    new Date("2000-10-1"),
+    new Date("2000-10-31"),
+    new Date("2000-11-1"),
+    new Date("2000-9-3"),
+    new Date("1900-10-11")
+];
+
+// 오늘이 2000년 10월 10일이라고 가정
+const today = new Date("2000-10-10/00:00:00");
+const filterArray = filterThisMonth(today, dateArray);
+
+console.log(filterArray);
+
+
